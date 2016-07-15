8884a2ccf019bcc9dace92bcbaa8ac1e0590b0e1
= <hmgaar@gmail.com>
2016-07-15 01:10:10 -0400

    added detox model

--- app/components/detox.component.ts
+++ app/components/detox.component.ts
@@ -1,27 +1,23 @@
 // Angular 2 objects
 import {Component, OnInit} from "@angular/core";
 
+// Model
+import {Detox} from '../models/detox.model';
+
 @Component({
   selector:'detox-component',
-  template:`
-     <StackLayout>
-        <Label [text]="welcome_msg" class="welcome_msg" textWrap="true"></Label>
-
-        <Label [text]="quote_of_day" class="quote" textWrap="true"></Label>
-         <Button text="TAP"></Button>
-    </StackLayout>
-  `,
+  templateUrl:'',
   styleUrls:["./components/detox.css"]
 })
 export class DetoxComponent implements OnInit {
     public welcome_msg: string = "Welcome to Quotox, you are not alone my friend!";
     public quote_of_day: string = "A Lion doesn't concern himself with the opinion of sheep."
-
+    public detox: Detox;
     constructor(){
 
     }
 
     ngOnInit(){
-
+      this.detox = new Detox();
     }
 }
