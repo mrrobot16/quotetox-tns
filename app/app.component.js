d59d44a0dc331d94a2a1316bf7bd18f556873f16
= <hmgaar@gmail.com>
2016-07-14 20:19:35 -0400

    added welcome message to our first screen

--- app/app.component.js
+++ app/app.component.js
@@ -2,27 +2,13 @@
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.counter = 16;
+        this.welcome_msg = "Welcome to Quotox, you are not alone!";
+        this.quote_of_day = "A lion is not concerned by the opinion of the sheep.";
     }
-    Object.defineProperty(AppComponent.prototype, "message", {
-        get: function () {
-            if (this.counter > 0) {
-                return this.counter + " taps left";
-            }
-            else {
-                return "Hoorraaay! \nYou are ready to start building!";
-            }
-        },
-        enumerable: true,
-        configurable: true
-    });
-    AppComponent.prototype.onTap = function () {
-        this.counter--;
-    };
     AppComponent = __decorate([
         core_1.Component({
             selector: "my-app",
-            templateUrl: "app.component.html",
+            template: "\n    <StackLayout>\n        <Label [text]=\"welcome_msg\" class=\"welcome_msg\"></Label>\n        <Button text=\"TAP\"></Button>\n        <Label [text]=\"quote_of_day\" class=\"quote\" textWrap=\"true\"></Label>\n    </StackLayout>\n    ",
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,20 +2,16 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: "my-app",
-    templateUrl: "app.component.html",
+    template: `
+    <StackLayout>
+        <Label [text]="welcome_msg" class="welcome_msg"></Label>
+        <Button text="TAP"></Button>
+        <Label [text]="quote_of_day" class="quote" textWrap="true"></Label>
+    </StackLayout>
+    `,
 })
 export class AppComponent {
-    public counter: number = 16;
+    public welcome_msg: string = "Welcome to Quotox, you are not alone!";
+    public quote_of_day: string = "A lion is not concerned by the opinion of the sheep."
 
-    public get message(): string {
-        if (this.counter > 0) {
-            return this.counter + " taps left";
-        } else {
-            return "Hoorraaay! \nYou are ready to start building!";
-        }
-    }
-
-    public onTap() {
-        this.counter--;
-    }
 }
