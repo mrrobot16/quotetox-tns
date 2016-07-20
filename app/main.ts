08d143cebcf6952b854116f093790e69bae41538
= <hmgaar@gmail.com>
2016-07-20 16:51:15 -0400

    added Datepicker to our Detox form

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import {nativeScriptBootstrap} from "nativescript-angular/application";
 import {AppComponent} from "./app.component";
+import {registerElement} from "nativescript-angular/element-registry";
+registerElement("DropDown", () => require("nativescript-drop-down/drop-down").DropDown);
 nativeScriptBootstrap(AppComponent, null, { startPageActionBarHidden: false });
