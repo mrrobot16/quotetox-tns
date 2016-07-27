ed525cec2751d4e448de1db02009bac915f031bd
= <hmgaar@gmail.com>
2016-07-27 01:14:18 -0400

    switch router deprecated to current router module

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,22 +1,22 @@
 // Angular 2 objects
 import {Component} from "@angular/core";
-import {RouteConfig} from '@angular/router-deprecated';
+// import {RouteConfig} from '@angular/router-deprecated';
 // import {NS_ROUTER_DIRECTIVES, NS_ROUTER_PROVIDERS} from 'nativescript-angular/router';
-import {NS_ROUTER_DIRECTIVES, NS_ROUTER_PROVIDERS} from "nativescript-angular/router-deprecated/ns-router-deprecated";
+import {NS_ROUTER_DIRECTIVES} from "nativescript-angular/router";
 // Components
 import {DetoxComponent} from './components/detox/detox.component';
 
 @Component({
     selector: "my-app",
     directives:[NS_ROUTER_DIRECTIVES],
-    providers:[NS_ROUTER_PROVIDERS],
+    // providers:[NS_ROUTER_PROVIDERS],
     template: `
   <page-router-outlet></page-router-outlet>
     `
 })
-@RouteConfig([
-  {path:'detox', name:'Detox', component:DetoxComponent, useAsDefault:true}
-])
+// @RouteConfig([
+//   {path:'detox', name:'Detox', component:DetoxComponent, useAsDefault:true}
+// ])
 export class AppComponent {
   public quote_of_day: string = "A Lion doesn't concern himself with the opinion of sheep."
   constructor(){
--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,12 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import {nativeScriptBootstrap} from "nativescript-angular/application";
+import {nsProvideRouter} from 'nativescript-angular/router';
+import {RouterConfig} from '@angular/router';
 import {AppComponent} from "./app.component";
-import {registerElement} from "nativescript-angular/element-registry";
-registerElement("DropDown", () => require("nativescript-drop-down/drop-down").DropDown);
-nativeScriptBootstrap(AppComponent, null, { startPageActionBarHidden: false });
+import {DetoxComponent} from './components/detox/detox.component';
+
+export const AppRoutes: RouterConfig = [
+{ path: "", component: DetoxComponent }
+]
+console.log("Hello Quotox");
+nativeScriptBootstrap(AppComponent, [[nsProvideRouter(AppRoutes,{})]], { startPageActionBarHidden: false });
