a81f689588264b695e25698a82c3d7780e2242d0
= <hmgaar@gmail.com>
2016-08-04 20:02:10 -0400

    added detox data to home-screen

--- app/components/home-screen/home-screen.component.ts
+++ app/components/home-screen/home-screen.component.ts
@@ -9,13 +9,15 @@ import {DetoxComponent} from '../detox/detox.component';
   selector:'home-screen-component',
   template:`
 <ActionBar title="Quotox">
-    <ActionItem text="Quotes" (tap)="quotes()" ios.position="left"></ActionItem>
+<!--    <ActionItem text="Quotes" (tap)="quotes()" ios.position="left"></ActionItem> -->
     <ActionItem text="Detox" (tap)="detox()" ios.position="right"></ActionItem>
 </ActionBar>
 
   <StackLayout>
      <Label text="Quote of the day" class="quote_of_day" textWrap="true"></Label>
      <Label [text]="quote_of_day" class="quote" textWrap="true"></Label>
+     <Label text="{{days_clean}} days free of alcohol" class="days_clean" textWrap="true"></Label>
+
   </StackLayout>
 
   `,
@@ -24,12 +26,16 @@ import {DetoxComponent} from '../detox/detox.component';
 })
 export class HomeScreenComponent implements OnInit {
     public quote_of_day: string;
+    public last_time: any = new Date(2016,0,17)
+    public today:any = new Date();
+    public days_clean:number;
     constructor(private router: Router){
-      this.quote_of_day = "A Lion doesn't concern himself with the opinion of a sheep.";
+
     }
 
     ngOnInit(){
-
+      this.quote_of_day = "A Lion doesn't concern himself with the opinion of a sheep.";
+      this.days_clean = Math.floor(((this.today - this.last_time)/(864*Math.pow(10,5))));
     }
 
     detox(){
